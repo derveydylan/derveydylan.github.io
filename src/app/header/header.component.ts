57715820bca0851c525baf965eb640caa845611f
Dervey Dylan <114241438+derveydylan@users.noreply.github.com>
2025-02-17 15:09:48 +0100

    feat: header nav menu on mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [RouterLink, RouterLinkActive],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    standalone: true,
+    imports: [RouterLink, RouterLinkActive],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent
+{
+    showMenu: boolean = false;
+    menuButtonText: string = "Menu"
 
+    toggleMenu(): void
+    {
+        const menu = document.getElementById("links");
+        const menuIcon = document.getElementById("menu-icon");
+
+        if(this.menuButtonText === "Menu")
+        {
+            this.menuButtonText = "Fermer";
+        }
+
+        else
+        {
+            this.menuButtonText = "Menu";
+        }
+
+        menuIcon?.classList.toggle("fa-bars");
+        menuIcon?.classList.toggle("fa-xmark");
+
+        menu?.classList.toggle("active-menu");
+    }
+
+    closeMenu(): void
+    {
+        const menu = document.getElementById("links");
+        const menuIcon = document.getElementById("menu-icon");
+
+        this.menuButtonText = "Menu";
+
+        menuIcon?.classList.add("fa-bars");
+        menuIcon?.classList.remove("fa-xmark");
+
+        menu?.classList.remove("active-menu");
+    }
 }
